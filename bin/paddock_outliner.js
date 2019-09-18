bcb2cefd32280d7222fb04826709559855dcaf73
ljs.upc.uq <ljs.upc.uq@gmail.com>
2019-09-18 12:14:05 +0000

    Updated bin/paddock_outliner.js (added 3 lines, deleted 151 lines).

--- bin/paddock_outliner.js
+++ bin/paddock_outliner.js
@@ -25,8 +25,6 @@ exports.initialise = function(app) {
   manager.app = app;
   manager.outlines = ui.Map.Layer();
   manager.selected = ui.Map.Layer();
-  
-  // li
   manager.elevation = ui.Map.Layer();
   // Save a soil layer to the app
   manager.soil = ui.Map.Layer();
@@ -43,20 +41,9 @@ var selectedVisParams = {
   palette:'FF0000'
 };
 
-
-//li 
-// var elevationVisParams = {
-// // black 
-//   palette:'#000000'
-// }
-
-//
-
 // Layer titles
 var LAYER_NAME_OUTLINES = 'All paddock outlines';
 var LAYER_NAME_SELECTED = 'Currently selected paddock: ';
-
-// li 
 var LAYER_NAME_ELEVATION = 'Elevation layer of selected paddock: ';
 
 // soil layer title
@@ -66,8 +53,6 @@ var LAYER_NAME_SOIL = 'Soil layer: ';
 // Setting these to false can speed up app performance.
 var SHOWN_OUTLINES = true;
 var SHOWN_SELECTED = true;
-
-// li
 var SHOWN_ELEVATION= true;
 
 // Setting soil layer to be shown automatically.
@@ -146,8 +131,6 @@ var setSelectedLayer = function() {
 };
 
 
-
-/// li 
 var setElevationLayer = function() {
   // Check if the data source for paddock outlines is empty
   if (manager.app.paddocks === null) {
@@ -159,36 +142,8 @@ var setElevationLayer = function() {
   //     manager.app.PROPERTY_SELECTED, 'equals', 1));
   
   
-  //TODO: Check if this set is empty before creating a layer out of it.
-  
   // Create a layer based off the currently selected paddocks
   var elevationOfSelectedPaddocks = ee.Image('CGIAR/SRTM90_V4');
-  
-  
-  /**var mosaic = ee.Image('CGIAR/SRTM90_V4').mosaic();
-  var elevationOfSelectedPaddocks = mosaic.clip(selectedPaddocks);
-  manager.soil = ui.Map.Layer({
-      eeObject: elevationOfSelectedPaddocks, 
-      name: LAYER_NAME_ELEVATION,
-      shown: SHOWN_ELEVATION,
-  });
-  */
-   
-  // var slope = ee.Terrain.slope(elevationOfSelectedPaddocks);
-  
-
-// // The region to reduce within.
-// var poly = manager.app.paddock.geometry();
-
-// // Reduce the image within the given region, using a reducer that
-// // computes the max pixel value.  We also specify the spatial
-// // resolution at which to perform the computation, in this case 200
-// // meters.
-// var max = elevationOfSelectedPaddocks.reduceRegion({
-//   reducer: ee.Reducer.max(),
-//   geometry: poly,
-//   scale: 200
-// });
   
   // manager.elevation = ui.Map.Layer({
   //     eeObject: elevationOfSelectedPaddocks, 
@@ -201,111 +156,8 @@ var setElevationLayer = function() {
       '#ffc623', '#ffa114','#ff5a0c']};
   
   manager.elevation = ui.Map.Layer(elevationOfSelectedPaddocks, visParams, "Elevation");
-  
   manager.elevation.setOpacity(0.5);
-  
-  
-  //https://developers.google.com/earth-engine/tutorial_api_03
-
-//   var image = ee.Image('CGIAR/SRTM90_V4');
-
-// // The region to reduce within.
-// var poly = ee.Geometry.Rectangle([-109.05, 41, -102.05, 37]);
-
-// // Reduce the image within the given region, using a reducer that
-// // computes the max pixel value.  We also specify the spatial
-// // resolution at which to perform the computation, in this case 200
-// // meters.
-// var max = image.reduceRegion({
-//   reducer: ee.Reducer.max(),
-//   geometry: poly,
-//   scale: 200
-// });
-
-// // Print the result (a Dictionary) to the console.
-// print(max);
-  
-  
-//   var PointsSelected = [
-//   ee.Feature(
-//       ee.Geometry.Point([145.8960858217797, -17.56668979348206]),
-//       {'name': 'point 1'}),
-//   ee.Feature(
-//       ee.Geometry.Point([145.89668123185277, -17.566176769199103]), 
-//       {'name': 'point 2'}),
-//   ee.Feature(
-//       ee.Geometry.Point([145.89865533768773, -17.566054026681186]),
-//       {'name': 'point 3'}),
-//   ee.Feature(
-//       ee.Geometry.Point([145.89865533768773, -17.566054026681186]),
-//       {'name': 'point 4'}),
-//   ee.Feature(
-//       ee.Geometry.Point([145.89923234664957, -17.565685718404545]), 
-//       {'name': 'point 5'})
-// ];
-
-// var PointsSelected = ee.FeatureCollection(PointsSelected);
-
-// var elevationTestChart = ui.Chart.image.byRegion({
-//   image: elevation,
-//   regions: PointsSelected,
-//   scale: 200,
-//   xProperty: 'name'
-// });
-
-// elevationTestChart.setOptions({
-//   title: 'Elevation test chart',
-//   vAxis: {
-//     title: 'Elevation (meters)'
-//   },
-//   legend: 'none',
-//   lineWidth: 1,
-//   pointSize: 4
-// });
-
-// print(elevationTestChart);
-
-// elevation = elevation.setName("hansen1"); 
-// elevation = elevation.setOpacity(0.5);
-
-// layer 1 = layer 1.setName("hansen1"); 
-// layer 1 = layer 1.setOpacity(0.5); 
-
-// Map.addLayer(elevation, {min: 500, max: 4500});
-// Map.addLayer(PointsSelected, {color: 'FF0000'});
-// Map.setCenter(145.89865533768773, -17.565685718404545, 11);
-  
-  
 };
-
-//   below is added by li   not sure whether it is right 
-  
-  
-//   var elevation = ee.Image('CGIAR/SRTM90_V4');
-
-// var allPointsSelected = [
-//   ee.Feature(
-//       ee.Geometry.Point([145.8960858217797, -17.56668979348206]),
-//       {'name': 'point 1'}),
-//   ee.Feature(
-//       ee.Geometry.Point([145.89668123185277, -17.566176769199103]), 
-//       {'name': 'point 2'}),
-//   ee.Feature(
-//       ee.Geometry.Point([145.89865533768773, -17.566054026681186]),
-//       {'name': 'point 3'}),
-//   ee.Feature(
-//       ee.Geometry.Point([145.89865533768773, -17.566054026681186]),
-//       {'name': 'point 4'}),
-//   ee.Feature(
-//       ee.Geometry.Point([145.89923234664957, -17.565685718404545]), 
-//       {'name': 'point 5'})
-// ];
-
-// var allPointsSelected = ee.FeatureCollection(allPointsSelected);
-
-
-//   above Li
-
 
 
 
