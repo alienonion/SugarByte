ebb36f3e09b79356d4a04ea85a30a4f8371b38aa
lonelyaliengg <lonelyaliengg@gmail.com>
2019-09-18 13:50:32 +0000

    Updated bin/ui_widgets/information_panel_factory.js (added 2 lines, deleted 2 lines).

--- bin/ui_widgets/information_panel_factory.js
+++ bin/ui_widgets/information_panel_factory.js
@@ -199,15 +199,15 @@ var createNDVIVisualiser = function(paddock) {
           date,
           // the paddock chosen by user
           paddock.geometry(),
-          'NDVI for paddock'+ paddock.get("ID"),
+          'NDVI for paddock'+ paddock.get("ID").evaluate(),
           true);
+          
       // Show a label with the date on the map.
       manager.time_label.setValue(new Date(xValue).toUTCString());
       debug.info("display NDVI imagery for paddock:", paddock.get("ID"));
       debug.info("added NDVI imagery to time series", date);
     })
   };
-
     ////////////////////////////
   // Visualise button
   var visualiseButton = ui.Button({
