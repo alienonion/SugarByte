2f92deb014131b4deea19d3e7a51f03aed718a13
champions0501 <champions0501@gmail.com>
2019-09-18 06:05:28 +0000

    Updated bin/paddock_outliner.js (added 3 lines, deleted 3 lines).

--- bin/paddock_outliner.js
+++ bin/paddock_outliner.js
@@ -156,14 +156,14 @@ var setElevationLayer = function() {
   // Filter to all the selected paddocks
   
   
-  // var selectedPaddocks = ee.FeatureCollection(ee.FeatureCollection(manager.app.paddocks).filterMetadata(
-  //     manager.app.PROPERTY_SELECTED, 'equals', 1));
+  var selectedPaddocks = ee.FeatureCollection(ee.FeatureCollection(manager.app.paddocks).filterMetadata(
+      manager.app.PROPERTY_SELECTED, 'equals', 1));
   
   
   //TODO: Check if this set is empty before creating a layer out of it.
   
   // Create a layer based off the currently selected paddocks
-  var elevationOfSelectedPaddocks = ee.Image('CGIAR/SRTM90_V4');
+  var elevationOfSelectedPaddocks = ee.Image('CGIAR/SRTM90_V4').mosaic().clip(selectedPaddocks);
   // var slope = ee.Terrain.slope(elevationOfSelectedPaddocks);
   
 
