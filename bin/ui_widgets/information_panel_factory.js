0eb8fd82fa7034a853dcb51ea7f197362f9ad8b8
lonelyaliengg <lonelyaliengg@gmail.com>
2019-09-19 03:18:18 +0000

    Updated bin/ui_widgets/information_panel_factory.js (added 2 lines).

--- bin/ui_widgets/information_panel_factory.js
+++ bin/ui_widgets/information_panel_factory.js
@@ -23,6 +23,8 @@ exports.initialise = function(app) {
   manager.app = app;// create a label to prompt users that points on map can be clicked to show the NDVI for that day on the map
   // no default layer select panel
   manager.layerSelectPanel = null;
+  //
+  manager.currentLayer = null;
 };
 
 /**
