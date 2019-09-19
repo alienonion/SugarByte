08470b9ea34acb646dbcb7715c4b28a276693689
lonelyaliengg <lonelyaliengg@gmail.com>
2019-09-19 04:11:00 +0000

    Updated bin/ui_widgets/information_panel_factory.js (added 1 line, deleted 1 line).

--- bin/ui_widgets/information_panel_factory.js
+++ bin/ui_widgets/information_panel_factory.js
@@ -93,7 +93,7 @@ var createSelectButton = function() {
     //   }
     // }
   });
-  manager.layerSelectPanel.add(manage.selectBoxTitle);
+  manager.layerSelectPanel.add(selectBoxTitle);
   manager.layerSelectPanel.add(manage.selectBox);
 }
 
