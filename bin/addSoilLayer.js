48799e0883aa933aff4553959bb718856eac7b9d
champions0501 <champions0501@gmail.com>
2019-09-19 09:48:30 +0000

    Deleted bin/addSoilLayer.js.

--- bin/addSoilLayer.js
+++
@@ -1,9 +0,0 @@
-/**** Start of imports. If edited, may not auto-convert in the playground. ****/
-var imageCollection = ee.ImageCollection("CSIRO/SLGA");
-/***** End of imports. If edited, may not auto-convert in the playground. *****/
-// Load Landsat 5 data, filter by date and bounds.
-var collection = ee.ImageCollection('CSIRO/SLGA')
-  .filterDate('2000-01-01', '2013-05-01');
-
-// Display the layer.
-Map.addLayer(collection);
