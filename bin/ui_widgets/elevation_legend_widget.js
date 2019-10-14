480f11e8fe680cccfae47fbd2e7f23e1222efe18
bald.dinosaur <bald.dinosaur@gmail.com>
2019-10-14 15:02:41 +0000

    Updated bin/ui_widgets/elevation_legend_widget.js (added 2 lines, deleted 6 lines).

--- bin/ui_widgets/elevation_legend_widget.js
+++ bin/ui_widgets/elevation_legend_widget.js
@@ -67,10 +67,6 @@ exports.initialise = function(app) {
 /**
  * Removes the elevation legend widget if it already exists.
  */
-exports.removeWidget = function () {
-    if (manager.legend != null) {
-        debug.info('Remove elevation legend widget. ');
-        Map.remove(manager.legend);
-        manager.legend = null;
-    }
+exports.removeWidget = function() {
+  manager.legend.style().set(shown, false);
 }
